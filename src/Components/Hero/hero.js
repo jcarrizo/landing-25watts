878a2d53bfb800006040ea49a17a559300fca795
Jesus Carrizo <jcarrizo15@gmail.com>
2021-11-20 12:01:52 -0300

    cambio de slider y arreglos

--- src/Components/Hero/hero.js
+++ src/Components/Hero/hero.js
@@ -1,21 +1,36 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import Slider from 'react-touch-drag-slider'
+import styled, { createGlobalStyle } from 'styled-components'
 import '../Hero/hero.css'
 import image from "../../Images/slide1-background@2x.png"
 import image2 from "../../Images/slide2-background@2x.png"
 import image3 from "../../Images/slide3-background@2x.png"
 import logo from "../../Images/logo_25w.svg"
 
+// json-server --watch db.json
+
 function Hero() {
 
+
+  const [images, setImages] = useState([])
 
   useEffect(() => {
     axios.get('http://localhost:3000/images')
       .then(function (response) {
         console.log(response.data);
+        setImages(response.data)
       }
       )
   }, [])
+
+
+  // The slider will fit any size container, lets go full screen...
+  const AppStyles = styled.main`
+  height: 100vh;
+  width: 100%;
+  object-fit: cover;
+`
 
 
 
@@ -51,60 +66,21 @@ function Hero() {
     </header>
     {/* <!-- hero section --> */}
     <section>
-      <div id="heroCarousel" className="carousel slide" data-bs-ride="carousel">
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src={image} className="d-block w-100 imagenes" alt="..."></img>
-            <div className="carousel-text">
-              <h3 className="fontSlider">Sed ut perspiciatis</h3>
-              <h3 className="fontSlider2">unde omnis iste natus</h3>
-              <p className="subfontSlider">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque.</p>
-              <button type="button" className="btn btn-hero btn-lg">Read more</button>
-              <div className="carousel-indicators">
-                <button type="button" data-bs-target="#heroCarousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#heroCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#heroCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      <AppStyles>
+        <Slider>
+          {images.map(({ url, title }, index) => (
+            <>
+              <img src={url} className="image-slider w-100" key={index} alt={title} ></img>
+              <div className="slider-text">
+                <h3 className="fontSlider">Sed ut perspiciatis</h3>
+                <h3 className="fontSlider2">unde omnis iste natus</h3>
+                <p className="subfontSlider">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque.</p>
+                <button type="button" className="btn btn-hero btn-lg">Read more</button>
               </div>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img src={image2} className="d-block w-100 imagenes" alt="..."></img>
-            <div className="carousel-text">
-              <h3 className="fontSlider">Proin elementum</h3>
-              <h3 className="fontSlider2">massa et felis</h3>
-              <p className="subfontSlider">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque.</p>
-              <button type="button" className="btn btn-hero btn-lg">Read more</button>
-              <div className="carousel-indicators">
-                <button type="button" data-bs-target="#heroCarousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#heroCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#heroCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-              </div>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img src={image3} className="d-block w-100 imagenes" alt="..."></img>
-            <div className="carousel-text">
-              <h3 className="fontSlider">Nulla rhoncus</h3>
-              <h3 className="fontSlider2">vulputate congue</h3>
-              <p className="subfontSlider">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque.</p>
-              <button type="button" className="btn btn-hero btn-lg">Read more</button>
-              <div className="carousel-indicators">
-                <button type="button" data-bs-target="#heroCarousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#heroCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#heroCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#heroCarousel" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#heroCarousel" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
+            </>
+          ))}
+        </Slider>
+      </AppStyles>
     </section>
   </>)
 }
