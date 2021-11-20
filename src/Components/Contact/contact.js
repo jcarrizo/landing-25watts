40da991c2ab8c47f4e627c5942fc166398ddb551
Jesus Carrizo <jcarrizo15@gmail.com>
2021-11-20 17:33:52 -0300

    validaciones en el formulario

--- src/Components/Contact/contact.js
+++ src/Components/Contact/contact.js
@@ -2,6 +2,33 @@ import React from 'react'
 import '../Contact/contact.css'
 import Image from '../../Images/image@2x.png'
 
+const submit = () => {
+  let name = document.getElementById("FormControlInputName").value
+  let email = document.getElementById("FormControlInputEmail").value
+  let number = document.getElementById("FormControlInputNumber").value
+  let textArea = document.getElementById("FormControlTextarea").value
+
+
+  if (name !== "" && email !== "" && number !== "" && textArea !== "") {
+    alert("tu nombre es: " + name)
+
+    alert("tu email es: " + email)
+
+    alert("tu numero es: " + number)
+
+    alert("tu mensaje es: " + textArea)
+
+
+    document.getElementById("FormControlInputName").value = ""
+    document.getElementById("FormControlInputEmail").value = ""
+    document.getElementById("FormControlInputNumber").value = ""
+    document.getElementById("FormControlTextarea").value = ""
+  }
+
+
+
+}
+
 
 function Contact() {
   return (<>
@@ -10,19 +37,22 @@ function Contact() {
         <div className="col-md-6 pe-5 columna1">
           <h2 className="titleAbout">Get in touch</h2>
           <p className="subtitleAbout">We are hiring!</p>
-          <div class="mb-3">
-            <input type="email" class="form-control" id="FormControlInputName" placeholder="Name"></input>
-          </div>
-          <div class="mb-3">
-            <input type="email" class="form-control" id="FormControlInputEmail" placeholder="Email"></input>
-          </div>
-          <div class="mb-3">
-            <input type="email" class="form-control" id="FormControlInputNumber" placeholder="Phone Number"></input>
-          </div>
-          <div class="mb-3">
-            <textarea class="form-control" id="FormControlTextarea" placeholder="Message" rows="3"></textarea>
-          </div>
-          <button type="button" className="btn btn-body btn-lg">Send</button>
+          <form onSubmit={() => { submit() }} >
+            <div className="mb-3">
+              <input type="text" className="form-control" id="FormControlInputName" maxLength="20" required placeholder="Name"></input>
+            </div>
+            <div className="mb-3">
+              <input type="email" className="form-control" id="FormControlInputEmail" maxLength="25" required placeholder="Email"></input>
+            </div>
+            <div className="mb-3">
+              <input type="number" className="form-control" id="FormControlInputNumber" min="1" max="999999999" required placeholder="Phone Number"></input>
+            </div>
+            <div className="mb-3">
+              <textarea className="form-control" id="FormControlTextarea" required placeholder="Message" maxLength="150" rows="3"></textarea>
+            </div>
+            <button type="submit" className="btn btn-body-contact btn-lg">Send</button>
+          </form>
+
         </div>
         <div className="col-md-6 d-flex align-items-center columna2">
           <div>
